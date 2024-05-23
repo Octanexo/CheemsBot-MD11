e50eb8f8f93f55841fe54643830f6382ed2ccfdc
Octanexo <167690379+Octanexo@users.noreply.github.com>
2024-05-23 17:19:45 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -14,24 +14,24 @@ const chalk = require('chalk')
 global.sessionid ='ur session id'
 
 //owmner v card
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.ytname = "YT: @IRONWILL786" //ur yt chanel name
+global.socialm = "GitHub: Sadistic_xc" //ur github or insta name
+global.location = "Pakistan, Punjab, Islamabad" //ur location
 
 //new
-global.botname = 'Cheems Bot MD V11' //ur bot name
-global.ownernumber = '916909137213' //ur owner number
-global.ownername = '🦄드림 가이 Xeon' //ur owner name
-global.websitex = "https://youtu.be/mu5m6aB6P5k"
-global.wagc = "https://whatsapp.com/channel/0029VaG9VfPKWEKk1rxTQD20"
+global.botname = 'REAPER' //ur bot name
+global.ownernumber = 923140152133' //ur owner number
+global.ownername = 'NOT ASIM MUNEER' //ur owner name
+global.websitex = "https://www.instagram.com/sadistic_xc?igsh=M2xreHJlNzQyZGI1"
+global.wagc = "https://chat.whatsapp.com/KCGNpk4Vs7EAO3F8NflXzF"
 global.themeemoji = '🪀'
-global.wm = "Xeon Bot Inc."
+global.wm = "NOT ASIM MUNEER."
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD11' //script link
-global.packname = "Sticker By"
-global.author = "🦄드림 가이 Xeon\n\n+916909137213"
-global.creator = "916909137213@s.whatsapp.net"
-global.xprefix = '.'
-global.premium = ["916909137213"] // Premium User
+global.packname = "PROPERTY OF"
+global.author = "NOT ASIM MUNEER\n\n+923140152133"
+global.creator = "923140152133@s.whatsapp.net"
+global.xprefix = '/'
+global.premium = ["923140152133"] // Premium User
 global.hituet = 0
 
 //bot sett
@@ -40,10 +40,10 @@ global.typereply = 'v2' // reply type 'v1' => 'v3'
 global.autoblocknumber = '92' //set autoblock country code
 global.antiforeignnumber = '91' //set anti foreign number country code
 global.welcome = false //welcome/left in groups
-global.anticall = false //bot blocks user when called
+global.anticall = true //bot blocks user when called
 global.autoswview = false //auto status/story view
-global.adminevent = false //show promote/demote message
-global.groupevent = false //show update messages in group chat
+global.adminevent = true //show promote/demote message
+global.groupevent = true //show update messages in group chat
 //msg
 global.mess = {
 	limit: 'Your limit is up!',
